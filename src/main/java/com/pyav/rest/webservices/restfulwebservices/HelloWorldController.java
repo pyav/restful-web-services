79b765a6890887af55e791413e06d51805ff48de
pyav <verma.anand09@gmail.com>
2021-05-02 23:29:21 +0530

    spring boot app

---
+++ src/main/java/com/pyav/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,32 @@
+package com.pyav.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping(path = "/hello-world-get")
+	public String helloWorldGet() {
+		return "Hello World Get";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("hello-world-bean");
+	}
+
+	@GetMapping(path = "/hello-world/{name}")
+	public String helloWorldPathVariable(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
+}

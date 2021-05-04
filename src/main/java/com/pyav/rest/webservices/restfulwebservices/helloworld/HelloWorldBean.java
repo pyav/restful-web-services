f6318a1a6bf7cea1c06f1fa9c351c9b2f8376420
pyav <verma.anand09@gmail.com>
2021-05-04 23:14:38 +0530

    new file

---
+++ src/main/java/com/pyav/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,8 @@
+Unstaged changes after reset:
+D	src/main/java/com/pyav/rest/webservices/restfulwebservices/HelloWorldBean.java
+M	src/main/java/com/pyav/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
+M	src/main/java/com/pyav/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+M	src/main/java/com/pyav/rest/webservices/restfulwebservices/user/User.java
+M	target/classes/META-INF/maven/com.pyav.restful-web-services/restful-web-services/pom.properties
+D	target/classes/com/pyav/rest/webservices/restfulwebservices/HelloWorldBean.class
+D	target/classes/com/pyav/rest/webservices/restfulwebservices/HelloWorldController.class

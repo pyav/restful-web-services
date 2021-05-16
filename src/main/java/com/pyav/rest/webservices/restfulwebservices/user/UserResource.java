4cdeccb5853b0311040bfbc7fac931ece8b16046
pyav <verma.anand09@gmail.com>
2021-05-16 22:26:48 +0530

    POST call returns new resource URI

--- src/main/java/com/pyav/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/pyav/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.pyav.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -20,16 +23,19 @@ public class UserResource {
 		return service.findAll();
 	}
 
-	@GetMapping(path = "/user/{id}")
+	@GetMapping(path = "/users/{id}")
 	public User getOneUser(@PathVariable Integer id) {
 		return service.findOne(id);
 	}
 
 	@PostMapping("/users")
-	//@CrossOrigin(origins = "*")
-	//@Bean(name="entityManagerFactory")
-	public void createUser(@RequestBody User user) {
+	// @CrossOrigin(origins = "*")
+	// @Bean(name="entityManagerFactory")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		System.out.println("Anand POST /users");
-		service.save(user);
+		User savedUser = service.save(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }

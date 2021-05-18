1d19c2b29e6c79d8b333aac0ff8f5a41fc1ecbd3
pyav <verma.anand09@gmail.com>
2021-05-18 23:11:38 +0530

    upodates

--- src/main/java/com/pyav/rest/webservices/restfulwebservices/exception/ExceptionResponse.java
+++ src/main/java/com/pyav/rest/webservices/restfulwebservices/exception/ExceptionResponse.java
@@ -6,11 +6,23 @@ public class ExceptionResponse {
 	private Date date;
 	private String message;
 	private String details;
-	
+
 	public ExceptionResponse(Date date, String message, String details) {
 		super();
 		this.date = date;
 		this.message = message;
 		this.details = details;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetails() {
+		return details;
 	}
 }

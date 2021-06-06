4270ebbfde8392c2abe79e403710f190a8d76c77
Anand <verma.anand09@gmail.com>
2021-06-06 22:28:50 +0530

    versioning through header

--- src/main/java/com/pyav/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/pyav/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -25,4 +25,15 @@ public class PersonVersioningController {
 	public PersonV2 paramV2() {
 		return new PersonV2(new Name("Anand", "pyav"));
 	}
+
+	@GetMapping(value="/person/header", headers="X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Anand pyav");
+	}
+
+	@GetMapping(value="/person/header", headers="X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Anand", "pyav"));
+	}
+	
 }

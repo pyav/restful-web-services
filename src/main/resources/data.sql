1a6fc39efdbee35830d23713b8f78b71d47247a2
Anand <verma.anand09@gmail.com>
2021-07-29 21:17:37 +0530

    many to one relation between posts and user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into user values (100, sysdate(), 'AB');
 insert into user values (200, sysdate(), 'Jill');
 insert into user values (300, sysdate(), 'Jack');
+insert into post values (1100, 'My first post', 100);
+insert into post values (1200, 'My second post', 100);
